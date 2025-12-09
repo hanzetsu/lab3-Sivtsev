10f379298c01adc7a3ce9cc774ff0e6201a921bf
Edward <eduardsivtsev@mail.ru>
2025-12-09 09:16:34 +0300

    Добавлен итератор и улучшена абстрактность

--- container.c
+++ container.c
@@ -1,5 +1,84 @@
 #include "container.h"
 
+struct node
+{
+    void *value;
+    struct node *prev;
+    struct node *next;
+};
+
+struct container
+{
+    struct node *head;
+    struct node *tail;
+    unsigned short size;
+    uint64_t elem_size;
+};
+
+struct Iterator
+{
+    struct node *element;
+};
+
+Iterator *iterator_begin(container *container)
+{
+    Iterator *iterator = malloc(sizeof(Iterator));
+    if (!iterator)
+    {
+        fprintf(stderr, "Не удалось выделить память под итератор\n");
+        exit(1);
+    }
+    iterator->element = container->head;
+    return iterator;
+}
+
+Iterator *iterator_end(container *container)
+{
+    Iterator *iterator = malloc(sizeof(Iterator));
+    if (!iterator)
+    {
+        fprintf(stderr, "Не удалось выделить память под итератор\n");
+        exit(1);
+    }
+    iterator->element = container->tail;
+    return iterator;
+}
+
+void iterator_destroy(Iterator *iterator)
+{
+    free(iterator);
+}
+
+void iterator_next(Iterator *iterator)
+{
+    if (iterator && iterator->element)
+    {
+        iterator->element = iterator->element->next;
+    }
+}
+
+void iterator_prev(Iterator *iterator)
+{
+    if (iterator && iterator->element)
+    {
+        iterator->element = iterator->element->prev;
+    }
+}
+
+int iterator_is_valid(const Iterator *iterator)
+{
+    return iterator != NULL && iterator->element != NULL;
+}
+
+void *iterator_get_value(Iterator *iterator)
+{
+    if (iterator_is_valid(iterator))
+    {
+        return iterator->element->value;
+    }
+    return NULL;
+}
+
 struct node *create_node(const void *value, uint64_t elem_size)
 {
     struct node *node = malloc(sizeof(struct node));
@@ -8,7 +87,7 @@ struct node *create_node(const void *value, uint64_t elem_size)
         fprintf(stderr, "Не удалось выделить память\n");
         exit(1);
     }
-    node->value = malloc(sizeof(elem_size));
+    node->value = malloc(elem_size);
     if (node->value == NULL)
     {
         fprintf(stderr, "Не удалось выделить память\n");
@@ -94,4 +173,70 @@ void container_delete_front(struct container *container)
         free(old_head);
     }
     container->size--;
+}
+
+void container_delete_back(struct container *container)
+{
+    if (container->size == 0)
+    {
+        fprintf(stderr, "Нет элементов в контейнере\n");
+        exit(1);
+    }
+    if (container->size == 1)
+    {
+        struct node *p = container->head;
+        container->head = NULL;
+        container->tail = NULL;
+        free(p->value);
+        free(p);
+    }
+    else
+    {
+        struct node *old_tail = container->tail;
+        struct node *new_tail = container->tail->prev;
+        container->tail = new_tail;
+        container->tail->next = NULL;
+        free(old_tail->value);
+        free(old_tail);
+    }
+    container->size--;
+}
+
+void container_clear(struct container *container)
+{
+    struct node *current = container->head;
+    while (current != NULL)
+    {
+        struct node *next = current->next;
+        free(current->value);
+        free(current);
+        current = next;
+    }
+    container->head = NULL;
+    container->tail = NULL;
+    container->size = 0;
+}
+
+unsigned short container_get_size(const container *container)
+{
+    return container->size;
+}
+
+void *container_get_index(container *c, unsigned short index)
+{
+    if (!c || index >= c->size)
+    {
+        fprintf(stderr, "Слишком большой индекс\n");
+        exit(1);
+    }
+
+    Iterator *iterator = iterator_begin(c);
+    for (unsigned short i = 0; i < index; ++i)
+    {
+        iterator_next(iterator);
+    }
+
+    void *value = iterator_get_value(iterator);
+    iterator_destroy(iterator);
+    return value;
 }
--- container.h
+++ container.h
@@ -1,19 +1,34 @@
 #pragma once
-#include "struct.h"
 #include <stdio.h>
-#include <stdlib.h>   
-#include <string.h>   
+#include <stdlib.h>
+#include <string.h>
 #include <stdint.h>
 
-struct node {
-    void *value;         
-    struct node *prev;     
-    struct node *next;     
-};
+typedef struct container container;
+typedef struct Iterator Iterator;
 
-struct container {
-    struct node *head;     
-    struct node *tail;     
-    unsigned short size;   
-    uint64_t elem_size;      
-};
+Iterator *iterator_begin(container *c);
+Iterator *iterator_end(container *c);
+
+void iterator_next(Iterator *it);
+void iterator_prev(Iterator *it);
+
+int iterator_is_valid(const Iterator *it);
+
+void *iterator_get_value(Iterator *it);
+
+void iterator_destroy(Iterator *it);
+
+void *container_get_index(container *c, unsigned short index);
+
+void container_init(container *c, uint64_t elem_size);
+
+void container_push_back(const void *value, container *c);
+void container_push_front(const void *value, container *c);
+
+void container_delete_front(container *c);
+void container_delete_back(container *c);
+
+void container_clear(container *c);
+
+unsigned short container_get_size(const container *c);
